ffb9ff3ad9123a7f76416bd1ab38c02fe3838e3c
kairuskill <33633907+kairuskill@users.noreply.github.com>
2018-09-29 16:29:00 -0300

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,2 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
 
+/**
+ *
+ * @author Leandro
+ */
+public class ConnectionFactory {
+    private static final String DRIVER = "con.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/nomedodatabese";
+    private static final String USER = "nomedousuario";
+    private static final String PASS = "senhadousuario";
+  
+    
+}
 
