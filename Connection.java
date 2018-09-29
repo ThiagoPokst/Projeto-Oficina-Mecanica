603eaf8cdd3799b0b379d2b8e104e9be34586790
kairuskill <33633907+kairuskill@users.noreply.github.com>
2018-09-29 17:02:14 -0300

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -10,11 +10,60 @@ package Connection;
  * @author Leandro
  */
 public class ConnectionFactory {
+    
+    //Variáveis 
     private static final String DRIVER = "con.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/nomedodatabese";
     private static final String USER = "nomedousuario";
     private static final String PASS = "senhadousuario";
   
+    public static Connection getConnection(){
+        
+        try {
+            Class.forName(DRIVER);
+            
+            return DriverManager.getConnection(URL, USER, PASS);
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexão", ex);
+        }  
+    }
     
+    public static void closeConnection(Connection con){
+        
+        if(con != null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro: "+ex);
+            }
+        }  
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+        
+        if(stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro: "+ex);
+            }
+        }
+        
+        closeConnection(con);
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+        
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                System.err.println("Erro: "+ex.getMessage());
+            }
+        }
+        
+        closeConnection(con, stmt);
+    }
 }
 
